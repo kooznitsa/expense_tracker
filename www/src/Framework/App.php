fd7847c929949605f68ebf833b24c38a59ac2023
Julia Kuznetsova <104004781+kooznitsa@users.noreply.github.com>
2024-09-20 18:09:03 +0300

    [feat] add routing (#2)

--- www/src/Framework/App.php
+++ www/src/Framework/App.php
@@ -6,8 +6,20 @@ namespace Framework;
 
 class App
 {
+    public function __construct(
+        private Router $router = new Router(),
+    ) {}
+
     public function run(): void
     {
-        echo "Application is running";
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, array $controller): void
+    {
+        $this->router->add("GET", $path, $controller);
     }
 }
