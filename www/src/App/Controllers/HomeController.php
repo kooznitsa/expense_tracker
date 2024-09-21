c76f13813bb58ba6e31dbf885e9ee457f3eb1e5f
Yulia Kuznetsova <kooznitsa@gmail.com>
2024-09-21 22:02:23 +0300

    [feat] add home page template

--- www/src/App/Controllers/HomeController.php
+++ www/src/App/Controllers/HomeController.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
 class HomeController
 {
+    public function __construct(
+        private TemplateEngine $view = new TemplateEngine(Paths::VIEW),
+    ) {}
+
     public function home(): void
     {
-        echo "Home page";
+        echo $this->view->render("/index.php", [
+            "title" => "Home page",
+        ]);
     }
 }
---
+++ www/src/App/views/partials/_footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="container mx-auto my-12 text-center">
+    &copy; Expense Tracker
+</footer>
+<!-- End Footer -->
