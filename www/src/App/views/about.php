b07a16659bc00e59a41d59a69cd1d55a3bdb9ca7
Yulia Kuznetsova <kooznitsa@gmail.com>
2024-09-23 10:42:25 +0300

    [feat] add autoloading routes

--- www/src/App/views/about.php
+++ www/src/App/views/about.php
@@ -4,11 +4,7 @@
 <section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded" >
     <!-- Page Title -->
     <h3>About Page</h3>
-
-    <hr />
-
-    <!-- Escaping Data -->
-    <p>Escaping Data: <?php echo e($dangerousData); ?> </p>
+    <p>This is a PHP app for tracking expenses.</p>
 </section>
 <!-- End Main Content Area -->
 
