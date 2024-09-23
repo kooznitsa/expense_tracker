3f897db35c0b00b2e54daeca85396c3f0ed6787f
Yulia Kuznetsova <kooznitsa@gmail.com>
2024-09-23 10:31:51 +0300

    [feat] add about page

--- www/src/App/functions.php
+++ www/src/App/functions.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 use JetBrains\PhpStorm\NoReturn;
 
+/*
+ * Prints a variable in user-friendly manner.
+ */
 #[NoReturn] function dd(mixed $value): void
 {
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     die();
+}
+
+/*
+ * Helps escaping data.
+ */
+function e(mixed $value): string
+{
+    return htmlspecialchars((string) $value);
 }
